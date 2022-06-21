1baf94a858845a548e214ce0927776223918515a
Leonardo Leite <leoinatel@gmail.com>
2022-06-21 18:23:35 -0300

    Cria pasta d1 do b4 e arquivos de acompanhamento do conteúdo.

---
+++ 1-fundamentos/b4-introdução-js-lógicadeprogramação/d1-js-primeiros-passos/variaveis.js
@@ -0,0 +1,10 @@
+var myName = "Leonardo";
+const birthCity = "Volta Redonda";
+let birthYear = 1985 ;
+
+birthYear = 2030;
+//birthCity = "TrybeCity"; A variável birthCity é uma Constante. 
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
