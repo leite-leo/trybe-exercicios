d2f2bf0c0195d927dd7684c041d12a6c931ed122
Leonardo Leite <leoinatel@gmail.com>
2022-06-26 15:04:06 -0300

    Atualiza com a conclusão do exercicio 7 (parte II - Funcoes)

--- 1-fundamentos/b4-introdução-js-lógicadeprogramação/d4-objetos-funcoes/pt2-exercicio7.js
+++ 1-fundamentos/b4-introdução-js-lógicadeprogramação/d4-objetos-funcoes/pt2-exercicio7.js
@@ -1,4 +1,19 @@
-function finalIgual(word, ending) {
+function verificaFimPalavra(word, ending) {
+  indexIgnore = word.length - ending.length;
+  aux = "";
+  result = true;
+  for ( i = indexIgnore; i < word.length; i +=1) {
+    aux +=word[i]; 
+  }
+  if (aux === ending) {
+    result = true;
+  } else {
+    result = false;
+  }
 
-
+  return console.log(result);
 }
+
+verificaFimPalavra("trybe", "be");
+verificaFimPalavra("paralelepipedo", "pipedo");
+
